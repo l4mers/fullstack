32c7518ae5d4b3457b36698cb087c416e9b8d3d9
Lamers <gustav@henrikssons.org>
2023-06-04 19:54:36 +0200

    asd

--- holidays/src/main/java/com/restservice/holidays/controllers/AmenityController.java
+++ holidays/src/main/java/com/restservice/holidays/controllers/AmenityController.java
@@ -1,4 +1,22 @@
 package com.restservice.holidays.controllers;
 
+import com.restservice.holidays.models.venue.Amenity;
+import com.restservice.holidays.repositories.AmenityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
 public class AmenityController {
+
+    private final AmenityRepository amenityRepository;
+
+    @GetMapping("get/amenities")
+    public ResponseEntity<Iterable<Amenity>> getAllAmenities(){
+        return ResponseEntity.ok(amenityRepository.findAll());
+    }
 }
