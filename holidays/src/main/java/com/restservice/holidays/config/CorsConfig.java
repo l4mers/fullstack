1d67619a846c7bc48a7c832a9f54f75257581187
Lamers <gustav@henrikssons.org>
2023-06-05 02:03:52 +0200

    mhm

--- holidays/src/main/java/com/restservice/holidays/config/CorsConfig.java
+++ holidays/src/main/java/com/restservice/holidays/config/CorsConfig.java
@@ -21,19 +21,30 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://fullstack-web-service-1:8080",
-                        "http://127.0.0.1:8080/api",
-                        "http://localhost:8080",
-                        "http://localhost",
-                        "http://fullstack-web-service-1",
-                        "fullstack-web-service-1:80",
-                        "fullstack-web-service-1",
-                        "fullstack-web-service-1:8080",
-                        "http://fullstack-holidays-1",
-                        "http://192.168.32.4",
-                        "http://192.168.32.4:80",
-                        "http://192.168.32.4:8080,",
-                        "http://localhost:3000")
+                .allowedOrigins("https://charming-dolphin-31618b.netlify.app/",
+                        "https://charming-dolphin-31618b.netlify.app",
+                        "http://127.0.0.1:5173/",
+                        "http://127.0.0.1:5173",
+                        "http://127.0.0.1",
+                        "http://127.0.0.1/")
+//                .allowedOrigins("http://fullstack-web-service-1:8080",
+//                        "http://127.0.0.1:8080/api",
+//                        "http://localhost:8080",
+//                        "http://localhost",
+//                        "http://fullstack-web-service-1",
+//                        "fullstack-web-service-1:80",
+//                        "fullstack-web-service-1",
+//                        "fullstack-web-service-1:8080",
+//                        "https://charming-dolphin-31618b.netlify.app/",
+//                        "http://fullstack-holidays-1",
+//                        "http://192.168.32.4",
+//                        "http://192.168.32.4:80",
+//                        "http://192.168.32.4:8080,",
+//                        "http://localhost:3000",
+//                        "https://holiday.imats.se",
+//                        "http://holiday.imats.se",
+//                        "https://holidays.imats.se",
+//                        "http://holidays.imats.se")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
